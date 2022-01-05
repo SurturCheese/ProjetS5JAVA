de4ee95fe348f4bd6f6dc1e5a3a1890230f27797
Selligre <92374805+Selligre@users.noreply.github.com>
2022-01-05 14:21:07 +0100

    Add files via upload

---
+++ pacman/GreenPellet.java
@@ -0,0 +1,12 @@
+package projetS5;
+
+import java.awt.Color;
+
+public class GreenPellet extends Pellet {
+
+    public GreenPellet(int posX, int posY) {
+        super(posX, posY);
+        this.color = Color.GREEN;
+        points = 1000;
+    }
+}
---
+++ pacman/TeleportPoint.java
@@ -0,0 +1,9 @@
+package projetS5;
+
+public class TeleportPoint extends Element {
+
+    public TeleportPoint(int posX, int posY) {
+        super(posX, posY);
+    }
+
+}
